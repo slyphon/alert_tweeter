5b978e2e68a12225bc381a72f038e1266a40849d
Jonathan D. Simms <slyphon@gmail.com>
2012-01-17 17:49:47 +0000

    w00t! works for services

--- lib/alert_tweeter.rb
+++ lib/alert_tweeter.rb
@@ -1,3 +1,5 @@
+$LOAD_PATH.unshift(File.expand_path('..', __FILE__)).uniq!
+
 require "alert_tweeter/version"
 
 require 'slop'
@@ -17,45 +19,119 @@ module AlertTweeter
   DEFAULT_CONFIG_PATH = File.expand_path('~/.alert_tweeter.yml')
 
   class Command
+    attr_reader :opts
 
-    def configure_client(path)
+    def self.run!(*a, &b)
+      new.run!(*a, &b)
+    end
+
+    def config
+      @config ||= YAML.load_file(opts[:config])
+    end
+
+    def notify_users
+      @config.fetch('notify_users')
+    end
+
+    def configure_client!(path)
       require 'yaml'
 
-      hash = YAML.load_file(path).fetch('auth')
+      hash = config.fetch('auth')
 
-      Twitter.configure do |config|
+      Twitter.configure do |c|
         %w[consumer_key consumer_secret oauth_token oauth_token_secret].each do |k|
-          config.__send__(:"#{k}=", hash.fetch(k))
+          c.__send__(:"#{k}=", hash.fetch(k))
         end
       end
     end
 
+    def timestamp
+      Time.at(timet.to_i).strftime('%Y-%m-%d %H:%M:%S')
+    end
+
+    def send_notification(message)
+      notify_users.each do |user|
+        Twitter.direct_message_create(user, message)
+      end
+    end
+
+    def service_notify!
+      send_notification <<-EOS.strip_heredoc[0...140]
+        #{notification_type} #{host_name}/#{service_desc} #{service_state}
+        #{timestamp}
+        #{service_output}
+      EOS
+    end
+
+    def host_notify!
+      send_notification <<-EOS.strip_heredoc[0...140]
+        #{notification_type} #{host_name} #{host_state}
+        #{timestamp}
+        #{host_output}
+      EOS
+    end
 
     def run!(*args)
       args << '-h' if args.empty?
 
-      @opts = Slop.parse!(args, :strict => true, :help => true) do
+      @opts = Slop.parse!(args, :strict => true) do
         banner <<-EOS.strip_heredoc
           Usage: #{File.basename($0)} [opts] 
 
           Will direct-message the configured users about service problems. Command-line options
           are provided that more-or-less match their nagios macro counterparts.
-
         EOS
 
-        on :f,  :config=,             'path to the config file to use', :default => DEFAULT_CONFIG_PATH
-        on :h,  :hostname=,           'host name where the alert is triggered', :required => true
-        on :s,  :servicedesc=,        'description of the service'
-        on      :servicestate=,       'string indicating the current state of the service'
-        on      :servicedurationsec=, 'number of seconds the service has been in the current state'
-        on      :serviceoutput=,      'first line of text output from the last service check'
-        on      :timet=,              'seconds since unix epoch'
+        on :f, :config,                 'path to the config file to use', true, :default => DEFAULT_CONFIG_PATH
 
-        on      :alert_type=,         'type of alert message: ACK, SERVICE, HOST', :match => /^(ACK|SERVICE|HOST)$/
+        on :n, :notification_type,      'string describing the type of notification being sent', true
 
+        on :H, :host_name,              'host name where the alert is triggered', true, :required => true
+        on :a, :host_address,           'IP address of the host', true
+        on :S, :host_state,             'UP, DOWN, or UNREACHABLE', true
+        on :Y, :host_state_type,        'HARD or SOFT', true
+        on :A, :host_attempt,           'number of the current re-check', true
+        on :O, :host_output,            'output from host check', true
 
+        on :d, :service_desc,           'description of the service', true
+        on :s, :service_state,          'string indicating the current state of the service', true
+        on :y, :service_state_type,     'HARD or SOFT', true
+        on :a, :service_attempt,        'number of the current re-check', true
+        on :u, :service_duration_sec,   'number of seconds the service has been in the current state', true
+        on :o, :service_output,         'first line of text output from the last service check', true
+
+        on :t, :timet,                  'seconds since unix epoch', true, :as => :integer
+        on :h, :help,                   "You're reading it", false, :tail => true
+      end
+
+      if opts.help?
+        $stderr.puts opts.help
+        exit 1
+      end
+
+      unless opts[:service_state] or opts[:host_state]
+        $stderr.puts "ERROR: You must specify either a --service_state or --host_state option"
+        exit 1
+      end
+
+      configure_client!(opts[:config])
+
+      if opts[:service_state]
+        service_notify!
+      else
+        host_notify!
       end
     end
+
+    private
+      def method_missing(name, *a, &b)
+        namesym = name.to_sym
+        if val = opts[namesym]
+          return val
+        else
+          super
+        end
+      end
   end
 end
 
